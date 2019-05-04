7ffa392934b3adf900ef6f96cacae1706d53edb3
huaiyongtai <huaiyongtai@gmail.com>
2019-05-04 10:57:01 +0800

    auto sync

--- worker/UltiSnips/t.php
+++ worker/UltiSnips/t.php
@@ -1,12 +1,2 @@
 <?php
 
-Today is the 04.05.19.
-Today is the .
-Today is the .
-Today is the .
-Today is the .
-Today is the 04.05.19.
-Today is the 19.05.19.
-Today is the 2019.05.19.
-Today is the 2019.05.04.
-Today is the 2019-05-04.
